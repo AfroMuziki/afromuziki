fcfc844ada12b4515c2c26bfbe9d612695d8efc5
AfroMuziki <info.afromuziki@gmail.com>
2026-03-29 21:24:08 +0300

    Create index.ts

---
+++ frontend/src/utils/formatters/index.ts
@@ -0,0 +1,4 @@
+// frontend/src/utils/formatters/index.ts
+export { formatNumber, formatDuration, formatTime } from './number';
+export { formatRelativeDate, formatDate, formatDateTime } from './date';
+export { formatFileSize } from './fileSize';
