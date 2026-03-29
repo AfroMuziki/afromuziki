ab57f9e02748ab46f08a46baa9e95b65971c1b6b
AfroMuziki <info.afromuziki@gmail.com>
2026-03-29 18:06:01 +0300

    Create index.ts

---
+++ frontend/src/hooks/content/index.ts
@@ -0,0 +1,10 @@
+// frontend/src/hooks/content/index.ts
+export { useContent } from './useContent';
+export { useContentList } from './useContentList';
+export { useInfiniteContent } from './useInfiniteContent';
+export { useTrending } from './useTrending';
+export { useNewReleases } from './useNewReleases';
+export { useUpload } from './useUpload';
+export { useDeleteContent } from './useDeleteContent';
+export { useEditContent } from './useEditContent';
+export { useBrowseContent } from './useBrowseContent';
