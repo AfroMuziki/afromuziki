2126126176f8523daf2010d5bdf1946ea0f836fe
AfroMuziki <info.afromuziki@gmail.com>
2026-03-29 18:09:01 +0300

    Create index.ts

---
+++ frontend/src/hooks/engagement/index.ts
@@ -0,0 +1,6 @@
+// frontend/src/hooks/engagement/index.ts
+export { useLike } from './useLike';
+export { useComments, useAddComment, useDeleteComment } from './useComments';
+export { useFollow } from './useFollow';
+export { useDownload } from './useDownload';
+export { useReport } from './useReport';
