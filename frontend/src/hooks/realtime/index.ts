2672589e11557cfab6e7d20ab87d99529f3e668a
AfroMuziki <info.afromuziki@gmail.com>
2026-03-30 07:28:59 +0300

    Create index.ts

---
+++ frontend/src/hooks/realtime/index.ts
@@ -0,0 +1,4 @@
+// frontend/src/hooks/realtime/index.ts
+export { useRealtimeNotifications } from './useRealtimeNotifications';
+export { useRealtimeComments } from './useRealtimeComments';
+export { useRealtimeFollowers } from './useRealtimeFollowers';
