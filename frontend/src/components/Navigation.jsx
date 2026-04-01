a5c46ae26b2ca611916c59427fe9fe4af8a92078
AfroMuziki <info.afromuziki@gmail.com>
2026-04-01 13:03:26 +0300

    Create Navigation.jsx

---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,8 @@
+// frontend/src/components/Navigation.jsx (add upload link)
+
+import { Link } from 'react-router-dom';
+
+// Add to your navigation component
+<Link to="/upload" className="nav-link">
+  Upload
+</Link>
